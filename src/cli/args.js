ff443fe9e6289ba5365359c396c5975f444027e1
bertasheugene <bertasheugene@gmail.com>
2022-06-05 21:33:49 +0300

    feat: complete cli parts

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+import { argv } from 'process';
+
 export const parseArgs = () => {
     // Write your code here 
+    let rssArgement = [];
+
+    for (let i = 0; i < argv.length; i++){
+        if(argv[i].indexOf('--') !== -1){
+            rssArgement.push(argv[i] + ' is ' + argv[i + 1]);
+        }
+    }
+    console.log(rssArgement.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
+
+import { argv } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+
+    // print process.argv
+
+    let rssArgement = [];
+
+    argv.forEach((val, index) => {
+        if(val.indexOf('RSS_') !== -1){
+            rssArgement.push(val);
+        }
+    });
+    console.log(rssArgement.join('; '));
 };
+
+parseEnv();
