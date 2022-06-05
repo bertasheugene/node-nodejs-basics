2d5ffbe175d8a92f0f980795472ededad4b45460
bertasheugene <bertasheugene@gmail.com>
2022-06-05 21:58:49 +0300

    feat: complete hash and modules parts

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,11 @@
+import crypto from "crypto";
+import { readFile } from "fs/promises";
+
 export const calculateHash = async () => {
     // Write your code here 
+    const fileContent = await readFile("files/fileToCalculateHashFor.txt");
+    const hachValue = crypto.createHash("sha256").update(fileContent).digest("hex");
+    console.log(hachValue);
 };
+
+calculateHash();
--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,31 +1,33 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { dirname, sep } from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import './files/c';
 
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = require("./files/a.json");
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = require("./files/b.json");
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path segment separator is "${sep}"`);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const createMyServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+  res.end("Request accepted");
 });
 
-module.exports = {
-    unknownObject,
-    createMyServer,
-};
-
+export { unknownObject, createMyServer };
